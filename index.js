9bd6cf41d0f7deb3ea16ed2a965e81f5e16b45f9
TrixOMG <nikitin.pavel.163@gmail.com>
2022-11-06 12:12:43 +0300

    formatted some code, removed wrong regExp

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ let addButton = document.getElementById("add-button");
 let input = document.getElementById("input");
 let todoContainer = document.getElementById("todo-container");
 
-let regExp = new RegExp(/\s/g);
+//let regExp = new RegExp(/\s/g);
 
 addButton.onclick = () => {
-  if (input.value.length == 0 || regExp.test(input.value)) {
-    alert("Please type your Task");
-  } else {
-    todoContainer.innerHTML += `
+	if (input.value.length == 0) {
+		//regExp.test(input.value)) {
+		alert("Please type your Task");
+	} else {
+		todoContainer.innerHTML += `
         <div class="task">
             <p>
                 ${input.value}
@@ -18,13 +19,13 @@ addButton.onclick = () => {
             </button>
         </div>
     `;
-  }
+	}
 
-  let tasks = document.querySelectorAll(".delete-button");
+	let tasks = document.querySelectorAll(".delete-button");
 
-  for (let i = 0; i < tasks.length; i++) {
-    tasks[i].onclick = () => {
-      tasks[i].parentNode.remove();
-    };
-  }
+	for (let i = 0; i < tasks.length; i++) {
+		tasks[i].onclick = () => {
+			tasks[i].parentNode.remove();
+		};
+	}
 };
